3f06610d76f47e0e3164a5b7b36c97c707c4bd7e
jefta-jose <ndegwajeff4@gmail.com>
2025-06-10 14:14:39 +0300

    test secret

--- server.js
+++ server.js
@@ -33,3 +33,11 @@ app.use('/api', timeRouter);
 app.listen(port, () => {
     Logger.info(`Server running on http://localhost:${port}`);
 });
+
+
+var pg_port=1212;
+var pg_host="gitguardians.com:9082/BLUDB";
+var pg_user="root";
+var pg_pass="sup3rstr0ngpass1ForGG";
+
+var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
